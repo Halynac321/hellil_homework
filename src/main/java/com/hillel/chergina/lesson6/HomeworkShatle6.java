f5a1e68788334d8d9b02be2e90345a31594f846d
GC <gc@GCs-MacBook-Pro.local>
2022-08-09 01:10:53 +0100

    correct version of homework

--- src/main/java/com/hillel/chergina/lesson6/HomeworkShatle6.java
+++ src/main/java/com/hillel/chergina/lesson6/HomeworkShatle6.java
@@ -2,19 +2,35 @@ package com.hillel.chergina.lesson6;
 
 public class HomeworkShatle6 {
     public static void main(String[] args) {
+        int a = 0;
             for (int i=1; i < 9; i++){
                 if ( i != 4 ) {
                     System.out.println(i);
+                    a++;
                 }
             }
             for (int j = 1; j < 9; j++) {
                 for (int k = 0; k < 9; k++) {
                     if ( k != 4 && j != 4) {
                         System.out.println(j +""+ k);
+                        a++;
                     }
                 }
             }
             System.out.println("100");
+            for (int j = 1; j < 9; j++) {
+                for (int k = 0; k < 9; k++) {
+                    for (int x = 0; x < 9; x++) {
+                        if ( x != 4 && k != 4 && j != 4) {
+                            System.out.println(j +""+ k + x);
+                            a++;
+                            if (a == 100) {
+                                System.exit(0);
+                            }
+                        }
+                    }
+                }
+            }
     }
 }
 
